5375aeb826591b059f7c54f7e3b1c3a93a37cef9
Bohdan Barylo <43064595+Bodyk1Love@users.noreply.github.com>
2019-03-16 16:29:45 +0200

    tets

---
+++ core_engine/init.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,request
+from transforming import summarize
+app = Flask(__name__)
+
+@app.route('/')
+def complete():
+   return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def summary():
+   text=request.json.get("text")
+   sentences=request.json.get("sentences")
+   percent=request.json.get("percent")
+   summ = summarize(text, percent)
+   return(summ, 201)
+
+if __name__ == '__main__':
+   app.run(debug = True)
