4309b92c25e41b37a1e587509fe053293fed6371
Bodyk1Love <vanya.zaliznyak@gmail.com>
2019-04-12 23:42:07 +0300

    DB LOGIN

--- Web Application/app.py
+++ Web Application/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_bootstrap import Bootstrap
+from transforming import summarize
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
@@ -40,7 +41,8 @@ class RegisterForm(FlaskForm):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -58,6 +60,14 @@ def login():
 
     return render_template('login.html', form=form)
 
+
+@app.route('/summarization', methods=['POST'])
+def summary():
+   text = request.form['text']
+   sent = request.form['sentence']
+   summ = summarize(text, sent)
+   return jsonify(summ)
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
@@ -73,16 +83,19 @@ def signup():
 
     return render_template('signup.html', form=form)
 
+
 @app.route('/dashboard')
 @login_required
 def dashboard():
     return render_template('dashboard.html', name=current_user.username)
+
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Web Application/static/js/main.js
+++ Web Application/static/js/main.js
@@ -3,6 +3,14 @@
 	$('#area').on('input', function (event) {
 	 	var text = event.target.value;
 	 	var sentences = text.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+	 	var amountOfSentences = sentences.length;
+	 	var lastLetter;
+	 	for(var i = 0;i<sentences.length;i++){
+	 		lastLetter = sentences[i][sentences[i].length-2];
+	 		if(lastLetter==lastLetter.toUpperCase() && sentences[i].length<3){
+	 			amountOfSentences-=1;
+	 		}
+	 	}
 	 	var form = document.getElementById("form");
 	 	console.log(sentences);
 
@@ -23,7 +31,7 @@
 		option.setAttribute('key',"select");
 		form.appendChild(option);
 
-		for(var i = 1;i<=sentences.length;i++){
+		for(var i = 1;i<=amountOfSentences;i++){
 			var option = document.createElement('option');
 			option.setAttribute('value',i);
 			option.innerHTML=i;
@@ -54,8 +62,15 @@
 				btn.disabled=false;
 			}
 		})
-		
-		
 
+		//ajax
+		$('#press').on('click', function(event) {
+			$(".res").show();
+			$(window).scrollTop($(".res").offset().top);
+			var data = $('#area').val();
+			document.getElementById("result").value = data;
+		});
 	})
+
 }());
+// дивитися речення і якшо там 1 символ то сплітити його до наступного
