7f12dbc0dbd104e0be58d26f53aacfbd1cace697
Bodyk1Love <vanya.zaliznyak@gmail.com>
2019-04-12 22:09:32 +0300

    Flask postgresql login

--- Web Application/app.py
+++ Web Application/app.py
@@ -28,14 +28,14 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)],render_kw={"placeholder":"Username"})
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)],render_kw={"placeholder":"Password"})
     remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)],render_kw={"placeholder":"Email"})
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)],render_kw={"placeholder":"Username"})
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)],render_kw={"placeholder":"Password"})
 
 
 @app.route('/')
